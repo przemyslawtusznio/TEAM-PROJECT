64034fcdd529fe3daf741cce8fee18f1b329466e
Bartosz <barkoz1@vp.pl>
2022-11-19 15:24:22 +0100

    Próba

--- teamproject/src/App.tsx
+++ teamproject/src/App.tsx
@@ -1,24 +1,17 @@
-<<<<<<< HEAD
-import React from 'react';
-import Navbar from './components/Navbar/Navbar';
-import LoginPage from './components/LoginPage/LoginPage';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import HomePage from './components/HomePage/HomePage';
-
-
-=======
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import LoginForm from "./components/LoginForm/LoginForm";
 import RegisterForm from "./components/RegisterForm/RegisterForm";
->>>>>>> 4177e607f2d24e9a044cdf46f029c7113f4884ff
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from "./components/HomePage/HomePage";
+import LoginPage from "./components/LoginPage/LoginPage";
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Navbar />
-<<<<<<< HEAD
       <Routes>
         <Route path='/' element = {<HomePage/>} />
         <Route path='/login' element= {<LoginPage/>} />
@@ -26,10 +19,6 @@ function App() {
       
       </BrowserRouter>
       
-=======
-      <LoginForm />
-      <RegisterForm />
->>>>>>> 4177e607f2d24e9a044cdf46f029c7113f4884ff
     </div>
   );
 }
--- teamproject/src/components/LoginForm/LoginForm.tsx
+++ teamproject/src/components/LoginForm/LoginForm.tsx
@@ -1,23 +1,20 @@
-<<<<<<< HEAD
 import React from 'react'
 import {Card, Typography, TextField, Button} from '@mui/material'
 import { useForm } from 'react-hook-form'
-import { LoginFormData } from '../helpers/Interfaces'
+import { LoginFormData } from '../../helpers/interfaces'
 import { signInWithEmailAndPassword, } from 'firebase/auth'
 import { auth } from '../../helpers/firebaseConfig'
 
 
-=======
-import React from "react";
-import { Card, Typography, TextField, Button } from "@mui/material";
-import { useForm } from "react-hook-form";
-import { LoginFormData } from "../../helpers/interfaces";
->>>>>>> 4177e607f2d24e9a044cdf46f029c7113f4884ff
 
 const LoginForm = () => {
   const { register, handleSubmit } = useForm<LoginFormData>();
 
-  const SubmitHandler = (data: LoginFormData) => {};
+  const SubmitHandler = (data: LoginFormData) => {
+
+    signInWithEmailAndPassword(auth, data.email, data.password)
+        .then(() => console.log("Login succesfull"))
+    }
 
   return (
     <Card sx={{ mb: "1rem" }}>
@@ -30,22 +27,7 @@ const LoginForm = () => {
         >
           Log In
         </Typography>
-
-<<<<<<< HEAD
-        signInWithEmailAndPassword(auth, data.email, data.password)
-        .then(() => console.log("Login succesfull"))
-    }
-    
-    return (   
-=======
-        <TextField
-          variant="outlined"
-          type="email"
-          placeholder="email"
-          sx={{ display: "block", mx: "auto", my: ".8rem" }}
-          {...register("email", { required: true })}
-        ></TextField>
->>>>>>> 4177e607f2d24e9a044cdf46f029c7113f4884ff
+ 
 
         <TextField
           variant="outlined"
@@ -69,3 +51,4 @@ const LoginForm = () => {
 };
 
 export default LoginForm;
+
