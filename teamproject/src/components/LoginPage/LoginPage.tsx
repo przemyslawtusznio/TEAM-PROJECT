9dcd3914e1d7cf60fc2e7822802a3fea3b2a4447
Jarosław Głowacki <yaroslav5800@gmail.com>
2022-11-19 15:30:01 +0100

    lf

--- teamproject/src/components/LoginPage/LoginPage.tsx
+++ teamproject/src/components/LoginPage/LoginPage.tsx
@@ -1,23 +1,28 @@
 import React from "react";
 import LoginForm from "../LoginForm/LoginForm";
-import { Typography, Button} from '@mui/material'
+import { Typography, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
-
 const LoginPage = () => {
-  
-  return ( <>
-    
-    <LoginForm/>
+  return (
+    <>
+      <LoginForm />
 
-    <Typography variant = 'h5' sx={{fontWeight: '80', textAlign: 'center'}}> Dont't You have an account yet? Register now.</Typography>
+      <Typography variant="h5" sx={{ fontWeight: "80", textAlign: "center" }}>
+        {" "}
+        Dont't You have an account yet? Register now.
+      </Typography>
 
-    
-    <Link to='/Register' style={{textDecoration: 'none'}}>
-    <Button variant = 'contained' sx={{mx: 'auto', display: 'block', my: '1rem'}}>Register</Button>
-    </Link>
+      <Link to="/Register" style={{ textDecoration: "none" }}>
+        <Button
+          variant="contained"
+          sx={{ mx: "auto", display: "block", my: "1rem" }}
+        >
+          Register
+        </Button>
+      </Link>
     </>
-  )
+  );
 };
 
 export default LoginPage;
